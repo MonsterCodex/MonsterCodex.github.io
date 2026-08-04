5d41ae7d681ae405d6a7a18b44b537393ca8b6ac
forgepilothq <leestevens1981@gmail.com>
2026-08-04 19:20:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+const monsters = [
 
+{
+name:"Siren Head",
+category:"Internet Horror",
+threat:"🔴 Extreme",
+image:"https://placehold.co/400x500/161B22/D32F2F?text=Siren+Head"
+},
+
+{
+name:"Slender Man",
+category:"Creepypasta",
+threat:"🔴 High",
+image:"https://placehold.co/400x500/161B22/D32F2F?text=Slender+Man"
+},
+
+{
+name:"Xenomorph",
+category:"Movies",
+threat:"🔴 Extreme",
+image:"https://placehold.co/400x500/161B22/D32F2F?text=Xenomorph"
+},
+
+{
+name:"Bigfoot",
+category:"Cryptid",
+threat:"🟠 Unknown",
+image:"https://placehold.co/400x500/161B22/D32F2F?text=Bigfoot"
+}
+
+];
+
+const grid = document.querySelector(".monster-grid");
+
+monsters.forEach(monster=>{
+
+grid.innerHTML += `
+
+<div class="card">
+
+<img src="${monster.image}" alt="${monster.name}">
+
+<div class="card-content">
+
+<h3>${monster.name}</h3>
+
+<p>${monster.category}</p>
+
+<p>${monster.threat}</p>
+
+<a href="#" class="button">View File</a>
+
+</div>
+
+</div>
+
+`;
+
+});
